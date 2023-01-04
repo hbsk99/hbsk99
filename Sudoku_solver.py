a983b374f599ca0f420b48e8ebc751cb5486f2ad
hbsk99 <42008638+hbsk99@users.noreply.github.com>
2023-01-05 01:37:59 +0530

    Add files via upload

---
+++ Sudoku_solver.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 23:24:04 2023
+
+@author: 00005301
+"""
+
+def is_a_correct_match(grid,row,col,number):
+   
+    for x in range(9):
+        if grid[row][x]==number:
+            return False
+    
+    for x in range(9):
+        if grid[x][col]==number:
+            return False
+    corner_row = row-row%3
+    corner_col = col-col%3
+    
+    for x in range(3):
+        for y in range(3):
+            if grid[corner_row+x][corner_col+y]==number:
+                return False
+    return True
+
+def solve(grid,row,col):
+    
+    if col==9:
+        if row==8:
+            return True
+        
+        row+=1
+        col=0
+   
+    if grid[row][col]>0:
+        
+        return solve(grid,row,col+1)
+   
+    for num in range(1,10):
+        if is_a_correct_match(grid,row,col,num):
+            grid[row][col]=num
+            if solve(grid,row,col+1):
+                return True
+        grid[row][col]=0
+    return False
+
+
+grid = [[0,7,1,0,4,0,3,0,0],
+        [2,0,3,5,0,0,0,1,9],
+        [0,0,0,0,0,3,0,2,0],
+        [0,0,0,0,7,2,9,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,6,8,5,0,0,0,0],
+        [0,1,0,4,0,0,0,0,0],
+        [3,9,0,0,0,8,5,0,4],
+        [0,0,5,0,2,0,1,6,0]]    
+
+if solve(grid,0,0):
+    for i in range(9):
+        for j in range(9):
+            print(grid[i][j],end=" ")
+        print()
+else:
+    print("No solution for this sudoku")
+     
